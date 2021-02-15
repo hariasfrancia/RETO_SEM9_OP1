d2017daf7f192a7883df012b272c1c192472515d
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-02-15 13:20:51 -0500

    update

---
+++ opcion1/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+          <html>
+              <body>
+                  <header>
+                      HEADER
+                  </header>
+                  <aside>
+                      <div>SIDENAV</div>
+                  </aside>
+                  <section>
+                      <div>
+                          <span>MAIN CONTENT</span>
+                      </div>
+                  </section>
+                  <aside id="derecha">
+                      <div>SIDENAV</div>
+                  </aside>
+                  <div class="limpiar"></div>
+ 
+                  <footer>
+                      Copyright | h@riasfrancia
+                  </footer>
+
+              </body>
+          </html>
+    </div>
+  )
+}
+
+
+
+
+
